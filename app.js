18cb5e94f845ae1d6662add0775d6de3e2addaec
Vijay <viijay1999@gmail.com>
2023-09-22 16:17:35 +0530

    Validated input box

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000
 mongoose.connect(process.env.MONGO_URI);
 
 const itemsSchema = {
-  name: String
+  name: String,
 };
 
 const Item = mongoose.model("Item", itemsSchema);
